e4aaa00c227335ffd7fd5187c6151c93c282b01e
DoNuTll40 <152863424+DoNuTll40@users.noreply.github.com>
2023-12-18 19:27:37 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,95 @@
+
+document.addEventListener('contextmenu', function (e) {
+  e.preventDefault();
+});
+
+const loginForm = document.querySelector('.login-form');
+const inp_username = document.querySelector('#username');
+const inp_password = document.querySelector('#password');
+const userlabel = document.querySelector('#userlabel');
+const passlabel = document.querySelector('#passlabel');
+const selectlabel = document.querySelector('#selectlabel');
+
+function coloruserLabel() {
+  inp_username.style.border = '3px solid red';
+  userlabel.style.textAlign = 'center';
+  userlabel.style.color = 'red';
+}
+
+function colorpassLabel() {
+  inp_password.style.border = '3px solid red';
+  passlabel.style.textAlign = 'center';
+  passlabel.style.color = 'red';
+}
+
+const checkUser = (inputObj) => {
+  const username = inputObj['username'].trim();
+  if (username === '') {
+    coloruserLabel();
+    userlabel.textContent = `กรุณาป้อนข้อมูลผู้ใช้!`;
+  } else if (username.includes(' ')) {
+    coloruserLabel();
+    userlabel.textContent = `ห้ามให้ชื่อมีช่องว่าง!`;
+  } else if (username.length <= 3) {
+    coloruserLabel();
+    userlabel.textContent = `ต้องมีมากกว่า 3 ตัวอักษร!`;
+  } else if (!isNaN(parseInt(username.charAt(0), 10))) {
+    coloruserLabel();
+    userlabel.textContent = `ห้ามใช้ชื่อที่มีตัวเลขขึ้นต้น!`;
+  } else {
+    checkPass(inputObj);
+    userlabel.textContent = ``;
+    inp_username.style.border = 'none';
+    return username;
+  }
+}
+
+const checkPass = (inputObj) => {
+  const password = inputObj['password'];
+  if (password === '') {
+    colorpassLabel();
+    passlabel.textContent = `กรุณาป้อนรหัสผ่าน!`;
+  } else if (password.includes(' ')) {
+    colorpassLabel();
+    passlabel.textContent = `ห้ามให้รหัสมีช่องว่าง!`;
+  } else if (password.length <= 4) {
+    colorpassLabel();
+    passlabel.textContent = `รหัสต้องมีมากกว่า 4 ตัวอักษร!`;
+  } else {
+    checkRole(inputObj);
+    passlabel.textContent = ``;
+    inp_password.style.border = 'none';
+    return password;
+  }
+};
+
+const checkRole = (inputObj) => {
+  const username = inputObj['username'];
+  const password = inputObj['password'];
+  const role = inputObj['role'];
+
+  if (role === 'Select Role'){
+    selectlabel.style.textAlign = 'center';
+    selectlabel.style.color = 'red';
+    selectlabel.textContent = `โปรดเลือกหน้าที่!`;
+    return;
+  }
+  selectlabel.textContent = ``;
+  alert('ข้อมูลที่คุณป้อนมาได้แก่' + '\nUsername : ' + username + '\nPassword : ' + password + '\nRole : ' + role);
+  location.href = 'https://example.com';
+}
+
+const validateInput = (inputObj) => {
+  checkUser(inputObj);
+};
+
+const hdlLogin = event => {
+    event.preventDefault();
+    let inputObj = {}
+    for(let event of loginForm.elements) {
+      inputObj[event.id] = event.value
+    }
+    validateInput(inputObj);
+}
+
+loginForm.addEventListener('submit', hdlLogin);
